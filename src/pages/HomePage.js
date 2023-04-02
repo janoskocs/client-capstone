cf75df48177caa05b4aebc2ebb78091eb94804bb
Janos Kocs <janos.kocs@outlook.com>
2023-04-02 20:56:09 +0100

    Create homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomePage = () => {
+  const [momentsList, setMomentsList] = useState(null);
+  useEffect(() => {
+    const getMoments = async () => {
+      try {
+        const { data } = await axios.get(
+          `${process.env.REACT_APP_SERVER_URL}/api/moments`
+        );
+        console.log(data);
+        setMomentsList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMoments();
+  }, []);
+
   return (
     <section className="home">
-      <h2>Home</h2>
+      <section className="moments">
+        {momentsList &&
+          momentsList.map((moment) => {
+            return <p key={moment._id}>{moment.title}</p>;
+          })}
+      </section>
     </section>
   );
 };
