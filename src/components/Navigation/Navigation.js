5ffb43b4f8f3da2aa32fa7d9c46b804a3d811edb
Janos Kocs <janos.kocs@outlook.com>
2023-04-03 21:05:46 +0100

    Implement logout button

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+
 const Navigation = () => {
+  const { logout } = useLogout();
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <nav className="nav">
       <Link to="/">
@@ -8,6 +14,8 @@ const Navigation = () => {
       </Link>
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
+
+      <button onClick={handleLogout}>Log out</button>
     </nav>
   );
 };
---
+++ src/hooks/useLogout.js
@@ -0,0 +1,12 @@
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+  const logout = () => {
+    //reset local storage and authContext to log out
+    localStorage.removeItem("user");
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return logout;
+};
