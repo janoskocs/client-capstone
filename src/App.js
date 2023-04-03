dec999b20d17075248b81a5788e11d529167bb64
Janos Kocs <janos.kocs@outlook.com>
2023-04-03 22:12:47 +0100

    Set up routes based on user logged in status

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { useAuthContext } from "./hooks/useAuthContext";
 import HomePage from "./pages/HomePage";
 import Navigation from "./components/Navigation/Navigation";
 import LogInPage from "./pages/LogInPage/LogInPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 const App = () => {
+  const { user } = useAuthContext();
+
   return (
     <>
       <div className="App">
         <BrowserRouter>
           <Navigation />
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LogInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
+            <Route
+              path="/"
+              element={user ? <HomePage /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <LogInPage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <SignUpPage /> : <Navigate to="/" />}
+            />
           </Routes>
         </BrowserRouter>
       </div>
