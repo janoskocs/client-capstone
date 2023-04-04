47b486a7f59d0b6f03e73b0b6c08ca82a3cb4edf
Janos Kocs <janos.kocs@outlook.com>
2023-04-04 23:01:13 +0100

    Create basic friend page

--- src/pages/FriendsPage/FriendsPage.js
+++ src/pages/FriendsPage/FriendsPage.js
@@ -1,5 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 const FriendsPage = () => {
-  return <div>FriendsPage</div>;
+  const { user } = useAuthContext();
+  const [allUsers, setAllUsers] = useState(null);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_SERVER_URL}api/user/allusers/`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      setAllUsers(data);
+    };
+
+    getUsers();
+  }, []);
+  console.log(allUsers, user);
+
+  return (
+    <section className="page">
+      <h2 className="page__title">Friends</h2>
+
+      <section className="find-followers">
+        <input type="text" placeholder="Find followers..." />
+        <button type="button">Search</button>
+
+        {allUsers &&
+          allUsers.map((user) => {
+            return <p>{user.first_name}</p>;
+          })}
+      </section>
+
+      <section className="my-followers"></section>
+    </section>
+  );
 };
 
 export default FriendsPage;
