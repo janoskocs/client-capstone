52349aa00ccde5ad4e2a5043cc412eed9208e20a
Janos Kocs <janos.kocs@outlook.com>
2023-04-10 11:31:58 +0100

    Add avatar url to token

--- src/pages/FollowerBoard/FollowerBoard.js
+++ src/pages/FollowerBoard/FollowerBoard.js
@@ -1,6 +1,9 @@
+import { useParams } from "react-router-dom";
 import "./FollowerBoard.scss";
 
 const FollowerBoard = () => {
+  const { followerId } = useParams();
+
   return <div>FollowerBoard</div>;
 };
 
