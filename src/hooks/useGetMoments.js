61ad23d6668af70a42a42d5be26b4993c73bb5f4
Janos Kocs <janos.kocs@outlook.com>
2023-04-10 13:24:03 +0100

    Implement fetch follower's board

--- src/hooks/useGetMoments.js
+++ src/hooks/useGetMoments.js
@@ -6,10 +6,10 @@ export const useGetMoments = () => {
   const { user } = useAuthContext();
   const [momentsList, setMomentsList] = useState(null);
 
-  const getMoments = async () => {
+  const getMoments = async (userId) => {
     try {
       const { data } = await axios.get(
-        `${process.env.REACT_APP_SERVER_URL}api/moments`,
+        `${process.env.REACT_APP_SERVER_URL}api/moments/${userId}`,
         {
           headers: {
             Authorization: `Bearer ${user.token}`,
--- src/pages/FollowerBoard/FollowerBoard.js
+++ src/pages/FollowerBoard/FollowerBoard.js
@@ -1,9 +1,22 @@
 import { useParams } from "react-router-dom";
 import "./FollowerBoard.scss";
+import { useGetMoments } from "../../hooks/useGetMoments";
+import { useEffect, useState } from "react";
 
 const FollowerBoard = () => {
+  const { momentsList, getMoments } = useGetMoments();
   const { followerId } = useParams();
 
+  useEffect(() => {
+    getMoments(followerId);
+  }, []);
+
+  if (!momentsList) {
+    console.log("notloaded");
+    return <p>Loading..</p>;
+  }
+
+  console.log(momentsList);
   return <div>FollowerBoard</div>;
 };
 
