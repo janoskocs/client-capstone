37adbad0d7f4b2599ba05779d3af61c510f513fc
Janos Kocs <janos.kocs@outlook.com>
2023-04-10 21:48:00 +0100

    Implement search new connections page

--- src/pages/FindFollowers/FindFollowers.js
+++ src/pages/FindFollowers/FindFollowers.js
@@ -5,40 +5,69 @@ import "./FindFollowers.scss";
 
 const FindFollowers = () => {
   const { user } = useAuthContext();
-  const [allUsers, setAllUsers] = useState(null);
-  const [searchResults, setSearchResults] = useState(
-    "Your search results will show up here."
-  );
+  const [input, setInput] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
 
   useEffect(() => {
     const getUsers = async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_SERVER_URL}api/user/allusers/`,
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}api/user/allusers/search`,
+        {
+          searchInput: input,
+        },
         {
           headers: {
             Authorization: `Bearer ${user.token}`,
           },
         }
       );
-      setAllUsers(data);
+      setSearchResults(data);
     };
 
+    if (!input) {
+      return;
+    }
     getUsers();
-  }, []);
+  }, [input]);
 
   return (
-    <section className="page">
-      <div className="page__top">
-        <h2 className="page__title">Find new connections</h2>
-        <input
-          className="page__input"
-          type="text"
-          placeholder="Start typing to find followers..."
-        />
-      </div>
+    <div className="wrapper">
+      <section className="page">
+        <div className="page__top">
+          <h2 className="page__title">Find new connections</h2>
+          <input
+            className="page__input"
+            type="text"
+            placeholder="Start typing to find people..."
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+        </div>
 
-      <section className="results">{searchResults}</section>
-    </section>
+        <section className="results">
+          {!input && <p>Your results will show up here.</p>}
+          {searchResults &&
+            searchResults.map((result) => {
+              return (
+                <article key={result._id} className="result">
+                  <div className="result__side">
+                    <img
+                      className="result__avatar"
+                      src={result.avatar}
+                      alt="User avatar"
+                    />
+                  </div>
+                  <div className="result__side">
+                    <p>{result.first_name}</p>
+                    <p>{result.last_name}</p>
+                  </div>
+                  <button>Follow</button>
+                </article>
+              );
+            })}
+        </section>
+      </section>
+    </div>
   );
 };
 
