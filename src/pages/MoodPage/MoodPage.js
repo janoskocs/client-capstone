e9290fb37d05303e3d03bba8d7d276f508729c37
Janos Kocs <janos.kocs@outlook.com>
2023-04-11 20:31:17 +0100

    Import chart library

--- src/pages/MoodPage/MoodPage.js
+++ src/pages/MoodPage/MoodPage.js
@@ -1,9 +1,17 @@
 import "./MoodPage.scss";
+import { LineChart, Line } from "recharts";
 
 const MoodPage = () => {
+  const data = [
+    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+    { name: "Page B", uv: 100, pv: 1300, amt: 500 },
+  ];
   return (
     <section className="mood">
       <div className="mood__wrapper">Mood page</div>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      </LineChart>
     </section>
   );
 };
